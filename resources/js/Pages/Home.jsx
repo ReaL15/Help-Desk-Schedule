1e7d990080e233a42827a576064cf9d4e6cf7678
Kelarkan <dhafaalvaro15@gmail.com>
2025-08-20 10:33:41 +0700

    first commit

---
+++ resources/js/Pages/Home.jsx
@@ -0,0 +1,7 @@
+export default function Home() {
+  return (
+    <div className="p-6">
+      <h1 className="text-2xl font-bold text-blue-600">Hello from Inertia + React (Laravel 12) 🚀</h1>
+    </div>
+  );
+}
